856e1b7c55792db64ef4f36b92ea24b764e30eaf
Pavel S <neutr00n90@gmail.com>
2023-05-09 00:01:49 +0300

    Configure build for GitHub Pages and clean up code

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 axios.defaults.baseURL =
-  "https://6457326f1a4c152cf97cf8a9.mockapi.io/users/api";
+  'https://6457326f1a4c152cf97cf8a9.mockapi.io/users/api';
 
 export async function getUsers(page = 1) {
   const searchParams = new URLSearchParams({
@@ -17,10 +17,5 @@ export async function updateUsers(id, followers) {
   const response = await axios.put(`/users/${id}`, {
     followers,
   });
-  return response.data;
-}
-
-export async function getUserById(id) {
-  const response = await axios.get(`/users/${id}`);
   return response.data;
 }
